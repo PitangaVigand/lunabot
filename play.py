12933cd6590d44889cc476c72cbe402342b3f621
PitangaVigand <pitangavigand@gmail.com>
2022-03-06 13:46:16 -0300

    Play_by_vision Under construction

--- play.py
+++ play.py
@@ -26,14 +26,21 @@ def play_by_time(range_team, energy):
         mouse_scroll(screen,team) 
         deselect_person(coord)
      
-play_by_time((4,5),1)
+play_by_time((0,5),1)
 
 
 def play_by_vision():
-    while True:
-
-
-        
-        #using class
-        return "ok"
+    view = True
+    while view:
+        #start
+        #print
+        play = True   
+        #count energy 
+        energy = 1
+        while play:                   
+            end_teams = fight_complete(screen,energy)
+            if end_teams:
+                play = False
+            return
+        view = False
 

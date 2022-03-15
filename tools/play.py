0c1454b863601b0157125db57b9a9d3edc8e10c0
PitangaVigand <pitangavigand@gmail.com>
2022-03-15 08:26:52 -0300

    Deprecate screenshot day

--- tools/play.py
+++ tools/play.py
@@ -1,4 +1,3 @@
-
 from msvcrt import locking
 import os
 import time
@@ -9,87 +8,75 @@ from selection import *
 from start import *
 
 
-
 def play(range_team, energy):
     print("------{0}-------------------".format(inspect.currentframe().f_code.co_name))
 
-    #screenshot day
-    if not os.path.exists("screenshots day/{}.jpg".format(date.today())):
-        screen_day()
-
-    #screenshot day
-    for i in range(0,3):
+    for i in range(0, 3):
         try:
             for team in range(*range_team):
                 screen = myscreen()
-                time.sleep(1) 
+                time.sleep(1)
                 coord = 0
 
-
-                for i in range(0,5):
-                    try:       
-                        mouse_scroll(screen,team) 
-                        time.sleep(1)
-                        screen= myscreen()         
-                        coord = select_person(screen,team)                
+                for i in range(0, 5):
+                    try:
+                        mouse_scroll(screen, team)
+                        screen = myscreen()
+                        coord = select_person(screen, team)
                         break
                     except Exception as e:
-                        time.sleep(3)#try
-                        print(e,"  Trying to select person...")
+                        time.sleep(3)  # try
+                        print(e, "  Trying to select person...")
                         continue
-                
 
-                fight_status = fight(screen,energy,coord)                        
-                
+                fight_status = fight(screen, energy, coord)
+
                 if not fight_status:
                     print("No fight in play!")
-                    time.sleep(3)#try
+                    time.sleep(3)  # try
                     return False
 
-                screen= myscreen()              
-                mouse_scroll(screen,team) 
+                screen = myscreen()
+                mouse_scroll(screen, team)
                 deselect_person(coord)
                 time.sleep(1)
-                
-            
-            print("------Finish {0}-------------------".format(inspect.currentframe().f_code.co_name))
+
+            print(
+                "------Finish {0}-------------------".format(
+                    inspect.currentframe().f_code.co_name
+                )
+            )
             time.sleep(1)
             return True
         except Exception as e:
-            time.sleep(3)#try
-            print(e,"  Trying put team to fight....")
+            time.sleep(3)  # try
+            print(e, "  Trying put team to fight....")
             continue
 
-def complete_play():
-    for i in range(0,3):
 
-        #Open to boss
+def complete_play():
+    for i in range(0, 3):
+
+        # Open to boss
         start_try = start()
         if start:
 
-            #select boss
-            for i in range(0,3):
-                screen = myscreen()    
-                boss = select_boss(screen)          
+            # select boss
+            for i in range(0, 3):
+
+                screen = myscreen()
+                boss = select_boss(screen)
                 if boss:
 
                     #####play####
-                    play_turn = play((0,5),1)
-                    if play_turn: 
+                    play_turn = play((0, 5), 3)
+                    if play_turn:
                         return True
-                    else:            
+                    else:
                         print("  Trying play....")
-                        time.sleep(3)#try
+                        time.sleep(3)  # try
                         continue
                 else:
-                    time.sleep(3)#try
+                    time.sleep(3)  # try
                     print("  Trying pick up boss")
                 continue
-
-        
-
-
-
-        
-
-    

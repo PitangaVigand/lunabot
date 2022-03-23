e79bb54d9f8ee6fac86b9a3a8badbd898d08ebb7
PitangaVigand <pitangavigand@gmail.com>
2022-03-23 14:54:19 -0300

    Rewrite print of next run

--- tools/run_bot.py
+++ tools/run_bot.py
@@ -7,11 +7,14 @@ from play import *
 
 
 def run_bot():
+    # to avoid problems of more than ano monitors
+
+    # start
     run = complete_play()
     time.sleep(1)
     if run:
         screen_day()
-        looking("button_voltar")
+        looking("button_back")
     print(
         "---------{0} complete!-------------------".format(
             inspect.currentframe().f_code.co_name
@@ -19,26 +22,22 @@ def run_bot():
     )
 
 
-# run_bot()
-# play(*args)
-play((1, 5), 3)
-# schedule.every(.03).minutes.do(run_bot)
+run_bot()
 schedule.every(4.5).hours.do(run_bot)
-
 while True:
     try:
         n = schedule.idle_seconds()
-        print(int(int(n) / 60), "minuts last")
+        if int(n / 60) % 60 == 0:
+            print(schedule.next_run().strftime("--------Next run %H:%M --------"))
+
     except Exception as e:
         print(e, "  in print status of bot")
-        time.sleep(3)  # try
+        time.sleep(3)  # try0503pitgui
 
     try:
         schedule.run_pending()
-        time.sleep(10)
+        time.sleep(5)
     except Exception as e:
         print(e, "  in run shcedule")
         time.sleep(3)  # try
         continue
-
-# complete_play()
